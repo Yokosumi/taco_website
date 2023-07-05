bf5b8a0e5c16b52d3265f55fa040d0b169e46c73
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-07-05 15:10:38 +0200

    latest state

--- src/pages/PageMenu.ts
+++ src/pages/PageMenu.ts
@@ -4,9 +4,6 @@ import "../styles/menu.scss";
 import images from "../data/images.json";
 
 export const PageMenu = () => {
-  const imageFoodRoll = images.find(
-    (imageItem) => imageItem.name === "food rolls"
-  );
   return /*html*/ `
 <main>
   <h1>Menu</h1>
@@ -14,10 +11,7 @@ export const PageMenu = () => {
   <div class="flexbox">
     ${MenuTable()}
 
-    <img src="${imageFoodRoll?.url}" alt="${imageFoodRoll?.alt}">
-
-
-    <img class="tall__image" src="https://assets.website-files.com/6196d22f14605588db7936dc/6196d918bdb49658c3e485b7_portrait-03.jpg"
+<img class="tall__image" src="https://assets.website-files.com/6196d22f14605588db7936dc/6196d918bdb49658c3e485b7_portrait-03.jpg"
       alt="french fries with an bowl of avocado dips with topings">
   </div>
   <img class="small__image" src="https://assets.website-files.com/6196d22f14605588db7936dc/6196d918bdb496f00ae485b8_landscape-07-p-500.jpeg"
